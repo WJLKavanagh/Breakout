8a1bf3dde789e859ea88befe458aa9f5827f64cf
wKavanagh <w.kavanagh@uad.ac.uk>
2024-06-01 18:32:58 +0100

    Add project files.

---
+++ Breakout/PowerupFireBall.cpp
@@ -0,0 +1,17 @@
+#include "PowerupFireBall.h"
+
+PowerupFireBall::PowerupFireBall(sf::RenderWindow* window, Paddle* paddle, Ball* ball)
+    : PowerupBase(window, paddle, ball)
+{
+    _sprite.setFillColor(ballEffectsColour);
+}
+
+PowerupFireBall::~PowerupFireBall()
+{
+}
+
+std::pair<POWERUPS, float> PowerupFireBall::applyEffect()
+{
+    _ball->setFireBall(5.0f);
+    return { fireBall, 5.0f };
+}
